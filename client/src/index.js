ab153903a459d2677471e23a79a7ccc172fc3122
GangHyeok Lee <rkdgur5381@gmail.com>
2023-10-17 16:54:12 +0900

    client server run currently, proxy setting, login feature

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware } from "redux";
+import promiseMiddleWare from 'redux-promise';
+import { createStore } from "redux";
+import Reducer from './_reducers';
+import ReduxThunk from 'redux-thunk';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleWare, ReduxThunk)(createStore);
+
 root.render(
-      <App />
-  );
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
+    <App />
+  </Provider>
+
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
