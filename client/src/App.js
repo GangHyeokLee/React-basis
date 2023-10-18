42e147f0f9d2787e4da173271ec5c9333571a295
GangHyeok Lee <rkdgur5381@gmail.com>
2023-10-18 17:40:01 +0900

    redux devtool이 안 됐던 이유: __ 안 적어줘서 작동 안 했던 것

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <Router>
-      <div>
         <Routes>
           <Route exact path="/" element={LandingPage()} />
 
@@ -15,8 +13,6 @@ function App() {
 
           <Route exact path="/register" element={RegisterPage()} />
         </Routes>
-      </div>
-    </Router>
   );
 }
 
